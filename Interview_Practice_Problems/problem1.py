50261f021a8d23fc3510a483195b6aad16857d31
vd141 <victor.dinh141@gmail.com>
2025-02-02 21:32:59 -0800

    problem description

--- Interview_Practice_Problems/problem1.py
+++ Interview_Practice_Problems/problem1.py
@@ -1,4 +1,9 @@
-def smaller_numbers_than_current(numbers)
+def smaller_numbers_than_current(numbers):
     '''
-    
+    takes a list of numbers as an argument
+
+    for each number determine how many numbers in the list are smaller than it,
+    and place the answer in a list. return the resulting list
+
+    only count unique values
     '''
